c80069eace4f0373c31490da3cc1d0e94b60cce4
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-08-16 10:58:37 +0100

    added playerShip

--- Source/ThirdPersonAsteroids/PlayerShipPawn.cpp
+++ Source/ThirdPersonAsteroids/PlayerShipPawn.cpp
@@ -2,12 +2,42 @@
 
 
 #include "PlayerShipPawn.h"
+#include "GameFramework/SpringArmCOmponent.h"
+#include "Camera/CameraComponent.h"
+#include "Camera/CameraShake.h"
+
+void APlayerShipPawn::CalculateMoveInput(float Value)
+{
+	MoveDirection = FVector(Value * MoveSpeed * GetWorld()->DeltaTimeSeconds, 0, 0);
+}
+
+void APlayerShipPawn::CalculateRoatateInput(float Value)
+{
+	float RotateAmount = Value * RotateSpeed * GetWorld()->DeltaTimeSeconds;
+	FRotator Rotation = FRotator(0, RotateAmount, 0);
+	RotationDirection = FQuat(Rotation);
+}
+
+void APlayerShipPawn::Move()
+{
+	AddActorLocalOffset(MoveDirection, true);
+}
+
+void APlayerShipPawn::Rotate()
+{
+	AddActorLocalRotation(RotationDirection, true);
+}
 
 // Sets default values
 APlayerShipPawn::APlayerShipPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
 
 }
 
@@ -15,20 +45,27 @@ APlayerShipPawn::APlayerShipPawn()
 void APlayerShipPawn::BeginPlay()
 {
 	Super::BeginPlay();
+
+
+	PlayerControllerRef = Cast<APlayerController>(GetController());
 	
-}
-
-// Called every frame
-void APlayerShipPawn::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void APlayerShipPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+
+void APlayerShipPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerShipPawn::HandleDestruction()
+{
 
 }
 
--- Source/ThirdPersonAsteroids/PlayerShipPawn.h
+++ Source/ThirdPersonAsteroids/PlayerShipPawn.h
@@ -5,13 +5,36 @@
 #include "CoreMinimal.h"
 #include "BaseShipPawn.h"
 #include "PlayerShipPawn.generated.h"
-
+class USpringArmComponent;
+class UCameraComponent;
+class UCameraShake;
 UCLASS()
 class THIRDPERSONASTEROIDS_API APlayerShipPawn : public ABaseShipPawn
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UCameraComponent* Camera;
 
+	FVector MoveDirection;
+	FQuat RotationDirection;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		float MoveSpeed = 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		float RotateSpeed = 100.0f;
+
+
+
+	APlayerController* PlayerControllerRef;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRoatateInput(float Value);
+
+	void Move();
+	void Rotate();
 public:
 	// Sets default values for this pawn's properties
 	APlayerShipPawn();
@@ -24,7 +47,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	virtual void HandleDestruction() override;
+
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
